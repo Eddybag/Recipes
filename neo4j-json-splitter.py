d3db50b02385826a7362ebc6d5051a82bc244af6
Edgardo <edgardo@edomac.local>
2018-04-15 20:40:54 -0300

    Cambio de json 200

--- neo4j-json-splitter.py
+++ neo4j-json-splitter.py
@@ -1,28 +1,14 @@
 import json
 
-<<<<<<< HEAD
 with open('recipes.json') as data_file:
 	data = json.load(data_file)
 lista = []
-for i, recipes in enumerate(data["recipes"]):
-	lista.append(recipes)
-	if(i % 2000 == 0):
+recipes = {}
+for i, r in enumerate(data["recipes"]):
+	lista.append(r)
+	if(i % 200 == 0):
 		with open('recipes'+str(i)+'.json', 'w') as outfile:
-			json.dump(lista, outfile)
+			recipes['recipes'] = lista
+			json.dump(recipes, outfile)
 		lista = []	
 
-
-
-
-=======
-with open('recipes.json') as data_file:    
-    data = json.load(data_file)
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-data = chunks(data, 3)
-print(data)
->>>>>>> fd790002da3cd8e76fbcc94a98a65d472ff264a4
