5d7dfcfabc08a910aebe54fceefeeebe81c8e3b0
Edgardo <edgardo@edomac.local>
2018-04-17 20:27:43 -0300

    ::

--- neo4j-json-splitter.py
+++ neo4j-json-splitter.py
@@ -4,11 +4,15 @@ with open('recipes.json') as data_file:
 	data = json.load(data_file)
 lista = []
 recipes = {}
-for i, r in enumerate(data["recipes"]):
-	lista.append(r)
-	if(i % 200 == 0):
+for i, r in enumerate(data["recipes"]):		
+	if(r['title'] is not None and r['title']):
+		lista.append(r)
+	if(r['time']['cookingMins'] is None):
+		r['time']['cookingMins']=0
+	if(r['time']['preparationMins'] is None):	
+		r['time']['preparationMins']=0
+	if(i % 50 == 0):
 		with open('recipes'+str(i)+'.json', 'w') as outfile:
-			recipes['recipes'] = lista
-			json.dump(recipes, outfile)
+			json.dump(lista, outfile)
 		lista = []	
 
