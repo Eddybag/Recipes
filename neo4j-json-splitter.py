b3342cbab00b0ab701a6e0349062d1e701f86a98
evelaguti <edg.velasquez.g@gmail.com>
2018-04-27 15:32:29 -0300

    Adding recipes by 25 each

--- neo4j-json-splitter.py
+++ neo4j-json-splitter.py
@@ -11,7 +11,7 @@ for i, r in enumerate(data["recipes"]):
 		r['time']['cookingMins']=0
 	if(r['time']['preparationMins'] is None):	
 		r['time']['preparationMins']=0
-	if(i % 50 == 0):
+	if(i % 25 == 0):
 		with open('recipes'+str(i)+'.json', 'w') as outfile:
 			json.dump(lista, outfile)
 		lista = []	
